4fbbad9337dd557871353646486d067f6a9ccbd6
OctA1N <navaneethprabhu3@gmail.com>
2022-10-17 17:47:52 +0530

    fixing css

--- config/connection.js
+++ config/connection.js
@@ -14,6 +14,16 @@ module.exports.connect=function(done) {
         done()
     })
 }
+// module.exports.connect=function(done) {
+//     const url='mongodb+srv://OctA1N:Mongodb2000@shoping.zrtbfpd.mongodb.net/?retryWrites=true&w=majority';
+//     const dbname='shopping'
+
+//     mongoClient.connect(url, (err, data) => {
+//         if(err) return done(err)
+//         state.db=data.db(dbname)
+//         done()
+//     })
+// }
 
 module.exports.get = function(){
     return state.db;
